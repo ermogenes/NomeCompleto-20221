37a1f3d47d4a10c303f3d43c6be18a77909e4ff0
Ermogenes Palacio <ermogenes.palacio@etec.sp.gov.br>
2022-03-11 22:06:07 -0300

    Finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome, sobrenome, sobrenomeEmMaiusculas;
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu segundo nome: ");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeEmMaiusculas = sobrenome.ToUpper();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+
+Console.WriteLine($"Nome de catálogo: {sobrenomeEmMaiusculas}, {nome}");
